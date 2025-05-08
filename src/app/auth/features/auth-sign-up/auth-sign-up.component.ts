8a425b5da84049a8c3cc4565b3e2074ec055fc21
Danny <leodanny024@gmail.com>
2025-05-07 20:32:17 -0600

    Auth Sign-up, Log-in, Conecction with Supabase, and Flowbite Component

---
+++ src/app/auth/features/auth-sign-up/auth-sign-up.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-sign-up',
+  imports: [],
+  templateUrl: `./auth-sign-up.html`,
+  styleUrl: './auth-sign-up.component.css'
+})
+export default class AuthSignUpComponent {
+
+}
---
+++ src/app/services/supabase.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SupabaseService {
+  private supabase: SupabaseClient;
+
+  constructor() { 
+      this.supabase = createClient('https://umphhhpeyguzpngdcyht.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcGhoaHBleWd1enBuZ2RjeWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxMzA4OTgsImV4cCI6MjA2MTcwNjg5OH0.yglWzoHjNJOSe2CllUHD3MT941mUvPnfiWKBNqQ9GdM');
+    }
+  
+    getProducts() {
+      return this.supabase.from('productos').select('*');
+    }
+  }
