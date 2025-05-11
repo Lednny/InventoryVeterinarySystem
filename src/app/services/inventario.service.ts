2f18ca288cf83ad7025e0141b517bb9613e8c250
Techxicn_ <jjangel2815@gmail.com>
2025-05-11 11:47:23 -0600

    Primer test de insercion de querys al proyecto, requiere revision

--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class InventarioService {
+export class InventoryService {
+  private supabase: SupabaseClient;
 
-  constructor() { }
+  constructor() {
+    this.supabase = createClient(
+      environment.SUPABASE_URL,
+      environment.SUPABASE_ANON_KEY
+    );
+  }
+
+  async getProductosPorSucursal(idSucursal: number) {
+    const { data, error } = await this.supabase
+      .from('stock')
+      .select('*, IdProducto(*), IdSucursal(*)')
+      .eq('IdSucursal', idSucursal);
+
+    if (error) throw error;
+    return data;
+  }
+
 }
