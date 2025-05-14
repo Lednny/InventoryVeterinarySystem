70bb0803e0a7819c8d106080747015214cebe86d
Danny <leodanny024@gmail.com>
2025-05-14 12:31:47 -0600

    Revert "Merge branch 'Querys' into Branch_V1"
    This reverts commit ff1394653de673fe7439a2030430af1c7a132cb5, reversing
    changes made to d9a97b23f360a07e7f9826e252e2ded9114ba219.

--- src/app/services/inventario.service.ts
+++ src/app/services/inventario.service.ts
@@ -1,28 +1,9 @@
 import { Injectable } from '@angular/core';
-import { createClient, SupabaseClient } from '@supabase/supabase-js';
-import { environment } from '../../environments/environment.development';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class InventoryService {
-  private supabase: SupabaseClient;
+export class InventarioService {
 
-  constructor() {
-    this.supabase = createClient(
-      environment.SUPABASE_URL,
-      environment.SUPABASE_ANON_KEY
-    );
-  }
-
-  async getProductosPorSucursal(idSucursal: number) {
-    const { data, error } = await this.supabase
-      .from('stock')
-      .select('*, IdProducto(*), IdSucursal(*)')
-      .eq('IdSucursal', idSucursal);
-
-    if (error) throw error;
-    return data;
-  }
-
+  constructor() { }
 }
