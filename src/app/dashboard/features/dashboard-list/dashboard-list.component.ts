e134a289a071a156013120a020eb7318f5e48730
Danny <leodanny024@gmail.com>
2025-05-28 02:52:54 -0600

    Sign-out Functional.

--- src/app/dashboard/features/dashboard-list/dashboard-list.component.ts
+++ src/app/dashboard/features/dashboard-list/dashboard-list.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component} from '@angular/core';
+import { Router, RouterModule} from '@angular/router';
+import { AuthService } from '../../../auth/data-access/auth.service';
 
 @Component({
   selector: 'app-dashboard-list',
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './dashboard-list.component.html',
   styleUrls: ['./dashboard-list.component.css'],
   standalone: true,
   
 })
 export default class DashboardListComponent {
+  showDropdown = false;
 
+  constructor(private router: Router, 
+  private authService: AuthService) {}
+
+  async signOut() {
+    this.showDropdown = false;
+    await this.authService.signOut();
+    this.router.navigate(['auth/log-in']);
+  }
 }
