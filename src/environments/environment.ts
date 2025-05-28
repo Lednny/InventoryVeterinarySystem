0c1132b027b47db8f7fb75633b43af2c51d10663
Danny <leodanny024@gmail.com>
2025-05-27 22:10:08 -0600

    Modifications in ..json

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    prodcution: true,
+    production: true,
     SUPABASE_URL: 'https://umphhhpeyguzpngdcyht.supabase.co',
     SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcGhoaHBleWd1enBuZ2RjeWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYxMzA4OTgsImV4cCI6MjA2MTcwNjg5OH0.yglWzoHjNJOSe2CllUHD3MT941mUvPnfiWKBNqQ9GdM',
 };
