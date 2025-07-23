2bb773761502f5d6a4e1fd700c33c8e4811a9f22
Danny <leodanny024@gmail.com>
2025-07-23 13:14:22 -0600

    env.

--- scripts/generate-env.js
+++ scripts/generate-env.js
@@ -1,11 +1,19 @@
+
 const fs = require('fs');
-const envPath = './src/environments/environment.prod.ts';
+const path = require('path');
+const envDir = './src/environments';
+const envPath = path.join(envDir, 'environment.prod.ts');
+
+// Crear la carpeta si no existe
+if (!fs.existsSync(envDir)) {
+  fs.mkdirSync(envDir, { recursive: true });
+}
 
 const content = `
 export const environment = {
-    production: true,
-    SUPABASE_URL: '${process.env.SUPABASE_URL}',
-    SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}'
+  production: true,
+  SUPABASE_URL: '${process.env.SUPABASE_URL}',
+  SUPABASE_ANON_KEY: '${process.env.SUPABASE_ANON_KEY}'
 };
 `;
 
