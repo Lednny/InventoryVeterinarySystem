ce0e3b3e49275649ce7d9e83be3d024e465d8fe3
Danny <leodanny024@gmail.com>
2025-10-07 15:36:53 -0600

    Adding SpeedInsights from vercel to checkout the metrics of the application. Added on app.component and index.html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { slideInAnimation } from './dashboard/animations/route-animations';
 import { APP_VERSION } from '../version';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -9,9 +10,16 @@ import { APP_VERSION } from '../version';
   styleUrl: './app.component.css',
   animations: [slideInAnimation]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'inventario-veterinario';
   version = APP_VERSION;
+
+  ngOnInit(){
+    injectSpeedInsights();
+
+    document.documentElement.classList.add('dark');
+    document.body.classList.add('bg-gray-900', 'text-white');
+  }
 
     getRouteAnimationData(outlet: RouterOutlet) {
     return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
